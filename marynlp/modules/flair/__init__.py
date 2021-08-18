1db5974857a7df1d3fe8c0afe30215219f05be6f
iam-kevin <kevin.al.james@gmail.com>
2021-08-18 17:52:43 +0300

    add appropriate `TextClassifier`

--- marynlp/modules/flair/__init__.py
+++ marynlp/modules/flair/__init__.py
@@ -3,8 +3,8 @@ from ..source import get_model_from_google_bucket, FLAIR_TEXT_CLASSIFIERS, FLAIR
 
 # Including this hear alone makes it such that you must have 
 # flair to be able to use this model
-from flair.models import SequenceTagger, TextClassifier
-
+from flair.models import SequenceTagger
+from .text_classifiers import TextClassifier
 
 # Model builders
 build_sequence_tagger: SequenceTagger = f.apply(SequenceTagger.load)(f.partial(get_model_from_google_bucket, flair_model_path_dict=FLAIR_SEQUENCE_TAGGERS))
